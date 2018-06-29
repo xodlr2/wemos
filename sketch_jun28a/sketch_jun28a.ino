3dcbc3be5a7bf5d4a3d4df6b95a1a10b9802498a
unknown <xodlr2@gmail.com>
2018-06-29 16:12:44 +0900

    server added

---
+++ sketch_jun28a/sketch_jun28a.ino
@@ -0,0 +1,70 @@
+#include <ESP8266WiFi.h>
+ 
+const char* ssid     = "Hello_IoT"; // "athena-2F";
+const char* password = "a7162008";
+ 
+WiFiServer server(80); //Initialize the server on Port 80
+
+int LED4 = 2;    // Use D4
+//int LED3 = 0;    // Use D3
+
+void setup() {
+  // put your setup code here, to run once:
+  WiFi.mode(WIFI_AP); //Our ESP8266-12E is an AccessPoint 
+  WiFi.softAP(ssid, password); // Provide the (SSID, password); . 
+  server.begin(); // Start the HTTP Server
+    
+  Serial.begin(115200); //Start communication between the ESP8266-12E and the monitor window
+  IPAddress HTTPS_ServerIP= WiFi.softAPIP(); // Obtain the IP of the Server 
+  Serial.println(" ");
+  Serial.print("Server IP is: "); // Print the IP to the monitor window 
+  Serial.println(HTTPS_ServerIP);
+  
+//  pinMode(LED3, OUTPUT);     // Initialize the LED pin as an output
+  pinMode(LED4, OUTPUT);     // Initialize the LED pin as an output
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  WiFiClient client = server.available();
+  if (!client) { 
+    return; 
+  } 
+  //Looking under the hood 
+  Serial.println("Somebody has connected :)");
+  
+  //Read what the browser has sent into a String class and print the request to the monitor
+  String request = client.readStringUntil('\r'); 
+  //Looking under the hood 
+  Serial.println(request);
+  
+  // Handle the Request
+  
+  if (request.indexOf("/OFF") != -1){ 
+    digitalWrite(LED4, HIGH); }
+  else if (request.indexOf("/ON") != -1){ 
+    digitalWrite(LED4, LOW); 
+  }
+
+  String s = "HTTP/1.1 200 OK\r\n";
+  s += "Content-Type: text/html\r\n\r\n";
+  s += "<!DOCTYPE HTML>\r\n<html>\r\n";
+  s += "<br><input type=\"button\" name=\"b1\" value=\"Turn LED ON\" onclick=\"location.href='/ON'\">";
+  s += "<br><br><br>";
+  s += "<input type=\"button\" name=\"b1\" value=\"Turn LED OFF\" onclick=\"location.href='/OFF'\">";
+  s += "</html>\n";
+  
+  client.flush(); //clear previous info in the stream
+  client.print(s); // Send the response to the client
+  delay(1);
+  Serial.println("Client disonnected"); //Looking under the hood
+  
+//  digitalWrite(LED4, LOW);   // Turn the LED
+//  digitalWrite(LED3, HIGH);   // Turn the LED
+//  delay(1000);    // Wait for a seconds
+//  digitalWrite(LED4, HIGH);  // Turn the LED
+//  digitalWrite(LED3, LOW);   // Turn the LED
+//  delay(1000);    // Wait for a seconds
+}
